0219be45c65843cc0bee6427a88ae4c4df2f9423
Ashish Dwivedi <ashishdwivedi.ece@gmail.com>
2020-04-18 21:18:08 -0400

    Renamed few files. Created placeholder for library file

---
+++ ffsr_spike_binary_lib.v
@@ -0,0 +1,13 @@
+`timescale 1ns / 1ps
+
+module ffsr_spike_binary_lib(rst, inc, dec, clk, out); 
+
+input wire rst;     //reset
+input wire inc;     //increment the value
+input wire dec;     //decrement the value
+output wire [2:0] out;    //current output
+input wire clk;
+
+// Library level implementation of ffsr, binary and spike encoding
+
+endmodule
