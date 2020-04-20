4c9bb3dd633424fa3b6d25ec3c6d4e08299be711
Ashish Dwivedi <ashishdwivedi.ece@gmail.com>
2020-04-20 02:18:13 -0400

    added optimized implementation of binary pulse FFSR. Added test file for the same

--- ffsr_pulse_binary_lib.v
+++ ffsr_pulse_binary_lib.v
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 
-module ffsr_pulse_binary_lib(rst, inc, dec, clk, out); 
+module ffsr_pulse_binary_opt (rst, inc, dec, clk, out); 
 
 input wire rst;     //reset
 input wire inc;     //increment the value
@@ -8,6 +8,26 @@ input wire dec;     //decrement the value
 output wire [2:0] out;    //current output
 input wire clk;
 
-// Library level implementation of ffsr, binary and pulse encoding
+reg [2:0] storage;
 
+wire [3:0] addition;
+
+assign addition = storage + {dec,dec,1'b1};
+
+wire carry_out = addition[3];
+
+assign sel = (carry_out & dec) | (inc & ~carry_out);
+
+always @(posedge clk or posedge rst)begin
+  if(rst) begin
+    storage <= 3'b0;
+  end
+  else begin
+    if(sel) begin
+      storage <= addition[2:0];
+    end
+  end
+end
+
+assign out = storage;
 endmodule
---
+++ ffsr_pulse_binary_test.sv
@@ -0,0 +1,73 @@
+// Pulse encoding testbench
+
+`timescale 1ns / 1ps
+
+module ffsr_pulse_binary_test;
+
+  logic rst;     //reset
+  logic inc;     //increment the value
+  logic dec;     //decrement the value
+  logic [2:0] out;    //current output
+  logic clk;
+  
+  ffsr_pulse_binary_opt DUT (.rst(rst), .inc(inc), .dec(dec),
+                  .clk(clk), .out(out));
+  
+  initial
+  begin
+      clk = 0;
+      $dumpfile("ffsr_pulse_binary_opt.vcd"); // Change this name as required
+      $dumpvars(0, ffsr_pulse_binary_test);
+      rst = 1'b0;
+      inc = 1'b0;
+      dec = 1'b0;
+      #5ns;
+      rst = 1'b1;
+      #40ns;
+      @(posedge clk);
+      #0;
+      rst = 1'b0;
+      inc = 1'b0;
+      dec = 1'b0;
+      @(posedge clk);
+      @(posedge clk);
+      @(posedge clk);
+      #0;
+      inc = 1'b1;
+      dec = 1'b0;
+      @(posedge clk);
+      @(posedge clk);
+      @(posedge clk);
+      @(posedge clk);
+      @(posedge clk);
+      @(posedge clk);
+      @(posedge clk);
+      @(posedge clk);
+      @(posedge clk);
+      @(posedge clk);
+      @(posedge clk);
+      @(posedge clk);
+      @(posedge clk);
+      @(posedge clk);
+      #0;
+      inc = 1'b0;
+      dec = 1'b1;
+      @(posedge clk);
+      @(posedge clk);
+      @(posedge clk);
+      @(posedge clk);
+      @(posedge clk);
+      @(posedge clk);
+      @(posedge clk);
+      @(posedge clk);
+      @(posedge clk);
+      @(posedge clk);
+      @(posedge clk);
+      @(posedge clk);
+      $finish;
+  end
+
+  always begin
+    #10ns clk = ~clk;
+  end
+endmodule
