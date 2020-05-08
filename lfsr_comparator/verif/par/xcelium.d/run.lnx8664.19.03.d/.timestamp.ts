c3eb99d8bdcd1fd01963841b577e14a088b84411
Santha Bhasuthkar <sbhasuth@ece000.ece.local.cmu.edu>
2020-05-08 00:05:59 -0400

    lfsr and comparator added

--- lfsr_comparator/verif/par/xcelium.d/run.lnx8664.19.03.d/.timestamp.ts
+++
@@ -1,4 +0,0 @@
-1404934139 /afs/ece.cmu.edu/class/ece725/gpdk045/libs/giolib045_v3.2/giolib045/vlog/pads_TT_s1vg.v
-1585458314 /afs/ece.cmu.edu/usr/sbhasuth/gcd_gpdk/syn/WORK/outputs/lfsr_behavioral_m.v
-1585682748 /afs/ece.cmu.edu/usr/sbhasuth/gcd_gpdk/rtl/lfsr_tb.sv
-1412090233 /afs/ece.cmu.edu/class/ece725/gpdk045/libs/gsclib045_svt_v4.4/gsclib045/verilog/fast_vdd1v0_basicCells.v
